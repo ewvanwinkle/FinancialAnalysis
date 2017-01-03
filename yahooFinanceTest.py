53449732d0b853c9271549317eb210622a4112d8
Erik Van Winkle <ewvanwinkle@gmail.com>
2017-01-03 13:42:47 -0500

    Delete yahooFinanceTest.py
    extraneous file which is no longer useful. we decided to use Pandas instead of yahoo-finance

--- yahooFinanceTest.py
+++
@@ -1,8 +0,0 @@
-from yahoo_finance import Share
-import csv
-
-with open('tickerSymbols.csv') as csvfile:
-    ticker = csv.reader(csvfile, delimiter = ',')
-    for row in ticker:
-        sharePrice = Share(row)
-        print row, sharePrice.get_open()
