e01f59e2db60dcd13fe0d3d9b3b882a189af7b94
Erik Van Winkle <ewvanwinkle@gmail.com>
2017-01-03 13:58:46 -0500

    Add files via upload
    slight update to include a csv file of the symbols that didnt correctly pull data

--- CreateSQLiteFinancialDatabase.py
+++ CreateSQLiteFinancialDatabase.py
@@ -13,11 +13,15 @@ with open('tickerSymbols.csv') as csvfile:
     ticker = csv.reader(csvfile, delimiter = ',')
 
     for row in ticker:
-        try:
-            # gathers data from the internet in a pandas DataFrame
-            temp = data.DataReader(row[0], 'yahoo', datetime(2014,1,6), datetime(2014,1,11))
 
-            # takes the pandas DataFrame and writes it to an sqlite table if it doesnt already exist
+        # for some reason, some of the ticker symbols just wouldnt recognize
+        try:
+            # gathers data from the internet in a pandas DataFrame.
+            # then writes this dataframe to an sqlite table
+            temp = data.DataReader(row[0], 'yahoo', datetime(2014,1,6), datetime(2014,1,11))
             temp.to_sql(row[0], cnx, if_exists = 'replace', flavor = 'sqlite')
+
         except:
-            pass
+            # records the symbols that didnt correctly pull data
+            with open('failedSymbols.csv', 'a') as myfile:
+                myfile.write('%s,' %row[0])
